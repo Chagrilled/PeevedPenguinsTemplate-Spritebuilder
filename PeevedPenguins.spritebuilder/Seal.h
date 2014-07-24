2fd33b28100e752826a3f1961cfecfcc9fe00b62
Andrew Reynolds <andrewreynolds@users-mbp-182.home>
2014-07-24 23:30:42 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by Andrew Reynolds on 24/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
