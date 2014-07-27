35e912aa9a318e37a42e74b149e22ecad448a895
Andrew Reynolds <andrewreynolds@users-mbp-182.home>
2014-07-27 12:24:32 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Andrew Reynolds on 27/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
